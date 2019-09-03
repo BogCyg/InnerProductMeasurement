610e2f25e7750996c810c2bdb38bb37e0d608714
BogCyg <32397686+BogCyg@users.noreply.github.com>
2019-09-03 13:44:56 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,20 @@
 ///////////////////////////////////////////////////////
-// BC++ book
+// Written by Boguslaw Cyganek, 2019
 ///////////////////////////////////////////////////////
-// by Boguslaw Cyganek, Wiley, 2019
+//
+// When using this code please cite the following paper:
+//
+// "How orthogonal are we? A note on fast and accurate 
+// inner product computation in the floating-point arithmetic"
+// by Boguslaw Cyganek and Kazimierz Wiatr 
+// First International Conference on SOCIETAL AUTOMATION
+// September 4-6, 2019, Krakow, Poland
+//
 ///////////////////////////////////////////////////////
 
 
 #include <iostream>
-#include <iomanip>
-#include <vector>
-#include <cassert>
-#include <filesystem>
-
-
-#include "..\..\ttmath\ttmath.h"
-
-
-
-
-using namespace std;
-
-namespace fs = std::experimental::filesystem;
-
-string	GetCurrentTime( void );
+#include <string>
 
 namespace InnerProducts
 {
@@ -30,65 +23,11 @@ namespace InnerProducts
 }
 
 
+using std::cout, std::endl, std::cin;
+using std::string;
 
-//////////////
-// ttmath test
-// Big<exponent, mantissa>
-typedef ttmath::Big<TTMATH_BITS(64), TTMATH_BITS(128)> MyBig;
+string	GetCurrentTime( void );
 
-// See
-// D:\Lunert\Precision Arithemtic Libs\TTMATH\ttmath-0.9.4.prerelease-src-2017.03.12\samples
-
-void TT_Test( void )
-{
-	MyBig	mb { 0 };
-
-	double qpi { 3.14151 };
-
-	mb.FromDouble( qpi );
-	//mb = qpi;
-
-	MyBig	sum { - 3.14151 };
-
-	sum += mb;
-
-
-	std::cout << "mb = " << mb.ToString() << ", qpi = " << qpi << endl;
-	std::cout << "sum = " << sum << endl;
-
-
-}
-
-//////////////
-
-void ES_Test( void )
-{
-	int ES_Test_main(int argn, char* argc[]);
-
-	char * arr[] = {
-						"",				// empty to simulate the environment
-						"1000",			// total elems to sum
-						"10",			// exponent difference
-						"3"				// what-to-do code
-	};
-
-
-	ES_Test_main( sizeof(arr)/sizeof(arr[0]), arr );
-
-}
-
-namespace InnerProducts
-{
-	namespace ES
-	{
-
-		auto DummyTest_908( void ) -> double;
-	}
-}
-
-//////////////
-
-int paranoia_main( void );
 
 //////////////
 
@@ -101,20 +40,8 @@ int main( void )
 	cout << "===========================" << endl << endl;
 
 
-	//paranoia_main();
-
-
-	//TT_Test();
-	//ES_Test();
-	//cout << "ES::DummyTest_908() = " << InnerProducts::ES::DummyTest_908() << endl;
-
-	//InnerProducts::InnerProduct_Test();
 	InnerProducts::InnerProduct_Test_GeneralExperiment();
 
-	char c {};
-	cin >> c;
-
-	return c;
 }
 
 
